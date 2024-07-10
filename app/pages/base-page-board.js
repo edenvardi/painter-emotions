42b3baabc4e3a76673a38eb8885e08a71537fdd6
edenvardi <edenvardi45@gmail.com>
2024-07-10 18:45:12 +0300

    fix clean board

--- app/pages/base-page-board.js
+++ app/pages/base-page-board.js
@@ -52,7 +52,7 @@ class BasePageBoard {
     cleanBoard() {
         print('CLEAN BOARD');
         board.background("#FF66C4");
-        board.rect(0, 0, windowWidth - 100, windowHeight - 200, 10);
+        board.rect(0, 0, windowWidth - 100, windowHeight - 270, 10);
         Object.values(this.brushes).forEach(b => b.clean());
     }
 
